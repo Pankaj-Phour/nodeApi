ea90b8610d074d3645e88d3c63e69864ce4be32c
Pankaj Phour <pankaj.phour70@gmail.com>
2023-04-11 12:43:02 +0530

    Page rendering method updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 // const { Binary } = require('mongodb');
 // const ThumbnailGenerator = require('video-thumbnail-generator');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 8000;
 app.use(cors())
 app.use(express.json({limit:"200mb"}));
 // app.use(express.urlencoded({ limit: "200mb", extended: true, parameterLimit: 50000 }))
@@ -27,11 +27,11 @@ const storage = multer.diskStorage({
   const upload = multer({ storage: storage })
 
 
-mongoose.connect('mongodb+srv://' + (process.env.MONGO).toString(),(req,res)=>{
+// mongoose.connect('mongodb+srv://' + (process.env.MONGO).toString(),(req,res)=>{
 
-    console.log("MongoDB connected",process.env.MONGO);
-    console.log(process.env.NAME);
-})
+    // console.log("MongoDB connected",process.env.MONGO);
+    // console.log(process.env.NAME);
+// })
 
 const video = mongoose.Schema({
     data: {
@@ -145,7 +145,8 @@ const record = mongoose.model('recodedVideo',recordings);
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use('/', require('./app.js'))
+// app.use('/', require('./app.js'))
+app.use(express.static('./pages'))
 
 app.get('/lessData', async(req,res)=>{
     let response = await model.find().lean();
