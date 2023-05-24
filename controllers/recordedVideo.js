da07a202a8b5a60e7e7ced4130b3f0d353d13943
Pankaj Phour <pankaj.phour70@gmail.com>
2023-05-24 10:57:46 +0530

    name and thumbnail added for user recorded video

--- controllers/recordedVideo.js
+++ controllers/recordedVideo.js
@@ -1,16 +1,19 @@
 const record = require('../models/recordingsModel.js');
 
-exports.recordedVideo = async (req,res)=>{
+exports.recordedVideo = async (req, res) => {
     const data = req.body;
     // console.log(data.url);
-    
+
     const video = new record({
-        url:data.url
+        url: data.url,
+        thumbnail: data.thumbnail,
+        Name: data.name,
     })
     video.save();
     res.status(200).send({
-        error:false,
-        message:'Video uploaded successfully',
-        response:data.url
+        code: 200,
+        error: false,
+        message: 'Video uploaded successfully',
+        response: data.url
     })
 }
